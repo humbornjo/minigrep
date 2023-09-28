e58e17812b0116d8d9d87a61e45d0d3c59ddd22d
humbornjo <humbornjo@gmail.com>
2023-09-28 23:42:31 +0800

    NEW: modulize main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::error::Error;
-use std::{env, fs, process};
+use minigrep::Config;
+use std::{env, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -10,32 +10,8 @@ fn main() {
     println!("serch for: {}", config.query);
     println!("in file: {}", config.file_path);
 
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("Application error: {e}");
         process::exit(1);
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
-    println!("With content: {contents}");
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-impl Config {
-    fn from(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-        Ok(Config {
-            query: query,
-            file_path: file_path,
-        })
     }
 }
